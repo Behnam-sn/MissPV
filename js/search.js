3ebb5219ad22f260cf152bb72e27d40dbdcd056c
Behnam Salehi <salehinezhad.b@gmail.com>
2018-08-05 14:14:46 +0430

    over

--- js/search.js
+++ js/search.js
@@ -1,17 +1,13 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+t = true;
 
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches(".dropbtn")) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// };
+function search() {
+  if (t == true) {
+    document.getElementById("icon").style.display = "none";
+    document.getElementById("search-input").style.display = "block";
+    t = false;
+  } else {
+    document.getElementById("icon").style.display = "block";
+    document.getElementById("search-input").style.display = "none";
+    t = true;
+  }
+}
